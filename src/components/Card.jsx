a5856747a0fde542dcb6499e172d2d0adda5690d
Maxim <79846131+vedmaxxx@users.noreply.github.com>
2023-08-30 17:17:26 +0500

    start

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <div>Название товара</div>
+      <div>Цена</div>
+      <div>Описание</div>
+      <div>Категория</div>
+      <div>Картинка</div>
+      <div>Рейтинг: оценка, кол-во оценок</div>
+    </div>
+  );
+};
+
+export default Card;
